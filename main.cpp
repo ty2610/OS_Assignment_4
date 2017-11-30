ced209936883aec0f6f5eef1a2da7b0a0ea4b786
Elena Ryan <elena.ryan@stthomas.edu>
2017-11-29 20:42:55 -0600

    process and mem allocated structs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+#include <map>
+
+
+
+using namespace std;
+
+struct VarMap {
+    map<string, char> cmap;
+    map<string, short> smap;
+    map<string, int> imap;
+    map<string, float> fmap;
+};//map struct
+
+struct Process {
+    int pid;
+    int code; //some number 2048 - 16384 bytes
+    int globals; //some number 0-1024 bytes
+    const int stack{65536}; //stack constant in bytes
+    VarMap nums;    
+};//Process struct
+
+    
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
