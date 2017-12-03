06381a91893aa300c9c32b98e95f5d537ad2089b
Tyler Green <gree0209@stthomas.edu>
2017-12-03 17:20:44 -0600

    added input handling and initial creation of memory

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <map>
+#include <ctime>
+#include <algorithm>
 
-
+//This is a CPP that will be compiled under c++ standard 11
+//compilable with g++ -o main main.cpp -std=c++11
 
 using namespace std;
 
@@ -12,23 +15,38 @@ struct VarMap {
     map<string, float> fmap;
 };//map struct
 
+struct MainInfo {
+    uint8_t *mem = new uint8_t[67108864];
+    int currentPID = 1024;
+} mainInfo;
+
+struct CommandInput {
+    int pageSize;
+}commandInput;
+
 struct Process {
     int pid;
     int code; //some number 2048 - 16384 bytes
     int globals; //some number 0-1024 bytes
     const int stack{65536}; //stack constant in bytes
-    VarMap nums;    
+    VarMap nums;
+    bool active; //if the process has been terminated or not
 };//Process struct
 
 const string COMMAND_NAME_EXIT = "exit";
 const string COMMAND_NAME_CREATE = "create";
 const string COMMAND_LINE_BREAK = "";
 
-int main() {
-    string input;
-    int id = 1024;
+void takeCommand(int argc, char *argv[]);
+bool isNumber(const string& s);
+void createProcess();
 
-    cout << "\nWelcome to the Memory Allocation Simulator! Using a page size of 8192 bytes.\n"
+int main(int argc, char *argv[]) {
+    string input;
+    int id = 1023;
+    srand( time( NULL ) );
+    takeCommand(argc,argv);
+    cout << "\nWelcome to the Memory Allocation Simulator! Using a page size of "<< commandInput.pageSize <<" bytes.\n"
             "Commands: \n"
             "* create (initializes a new process)\n"
             "  * allocate <PID> <var_name> <data_type> <number_of_elements> (allocated memory on the heap)\n"
@@ -42,22 +60,14 @@ int main() {
             "    * if <object> is a \"<PID>:<var_name>\", print the value of the variable for that process" << endl;
 
     while(true){
-        cout << "> ";
+        cout << ">  ";
 
         getline(cin,input);
 
         if(input == COMMAND_NAME_EXIT){
             break;
         }else if (input == COMMAND_NAME_CREATE){
-            Process *process = new Process;
-            process->pid = id++;
-            process->code = rand()% 14337 + 2048; //2048-16384
-            process->globals= rand()% 1024; //0-1024
-
-            cout << process->pid << endl;
-            //cout << process->code << endl;
-            //cout << process->globals << endl;
-            //cout << process->stack << endl;
+            createProcess();
         }else if (input == COMMAND_LINE_BREAK){
           //do nothing
         } else{
@@ -66,4 +76,61 @@ int main() {
 
     }
     return 0;
+}
+
+//String to int checker to make sure it is valid
+//Goes character by character and makes sure it is 0-9
+//https://stackoverflow.com/questions/4654636/how-to-determine-if-a-string-is-a-number-with-c
+bool isNumber(const string& s) {
+    return !s.empty() && find_if(s.begin(),
+                                 s.end(), [](char c) { return !std::isdigit(c); }) == s.end();
+}
+
+void takeCommand(int argc, char *argv[]) {
+    string commandParam = "-p";
+    if(argc > 1){
+        if(argc < 4){
+            if(string(argv[1]) == "-p") {
+                if(isNumber(string(argv[2]))) {
+                    int pageHolder = stoi(string(argv[2]));
+                    if(pageHolder>1023 && pageHolder<32769){
+                        //used link below to see if an integer is a power of 2
+                        //https://stackoverflow.com/questions/108318/whats-the-simplest-way-to-test-whether-a-number-is-a-power-of-2-in-c
+                        if((pageHolder & (pageHolder - 1)) == 0){
+                            commandInput.pageSize = pageHolder;
+                        } else {
+                            cout << "The page size must be a power of two" << endl;
+                            exit(0);
+                        }
+                    } else {
+                        cout << "The page size must be between 1024 and 32768" << endl;
+                        exit(1);
+                    }
+                } else {
+                    cout << "Your page number must be an integer" << endl;
+                    exit(2);
+                }
+            } else {
+                cout << "You can only provide the -p argument" << endl;
+                exit(3);
+            }
+        } else {
+            cout << "You can only provide one argument" << endl;
+            exit(4);
+        }
+    } else {
+        cout << "You must provide a page size argument" << endl;
+        exit(5);
+    }
+}
+
+void createProcess(){
+    Process *process = new Process;
+    process->pid = mainInfo.currentPID;
+    mainInfo.currentPID++;
+    process->code = rand()% 14337 + 2048; //2048-16384
+    process->globals= rand()% 1025; //0-1024
+    process->active = true;
+
+    cout << process->pid << endl;
 }
